200bc6171fafb4383c3c570ea469ab0dfd44682c
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-24 18:55:19 -0800

    a

--- ArmPi/Functions/W7Perception.py
+++ ArmPi/Functions/W7Perception.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 # coding=utf8
 import sys
-sys.path.append('/home/pi/ArmPi/')
 import cv2
-import time
 import Camera
-import threading
-from LABConfig import *
-from ArmIK.Transform import *
-from ArmIK.ArmMoveIK import *
-import HiwonderSDK.Board as Board
-from CameraCalibration.CalibrationConfig import *
 import numpy as np
 
 if sys.version_info.major == 2:
@@ -27,104 +19,65 @@ range_rgb = {
     'white': (255, 255, 255),
 }
 
-# Set the detection color
+# Function to set the target color
 def setTargetColor(target_color):
     global __target_color
     __target_color = target_color
     return (True, ())
 
 # Find the contour with the maximum area
-# Parameter is a list of contours to compare
 def getAreaMaxContour(contours):
-    contour_area_temp = 0
     contour_area_max = 0
     area_max_contour = None
 
-    for c in contours:  # Iterate through all contours
-        contour_area_temp = math.fabs(cv2.contourArea(c))  # Calculate contour area
+    for c in contours:
+        contour_area_temp = cv2.contourArea(c)
         if contour_area_temp > contour_area_max:
             contour_area_max = contour_area_temp
-            if contour_area_temp > 300:  # Only contours with an area greater than 300 are considered valid to filter out noise
+            if contour_area_temp > 300:
                 area_max_contour = c
 
-    return area_max_contour, contour_area_max  # Return the largest contour
+    return area_max_contour, contour_area_max
 
-rect = None
-size = (640, 480)
-rotation_angle = 0
-unreachable = False
-world_X, world_Y = 0, 0
-world_x, world_y = 0, 0
-
-# App initialization call
-def init():
-    print("ColorTracking Init")
-
-# App start call
-def start():
-    print("ColorTracking Start")
-
-# App stop call
-def stop():
-    print("ColorTracking Stop")
-
-# App exit call
-def exit():
-    print("ColorTracking Exit")
-
-def run(img,roi):
-    global rect
+def run(img, roi):
     global __target_color
-    global size
-    global rotation_angle
-    global world_X, world_Y
-    global world_x, world_y
+    global range_rgb
 
-    roi = False
-    Frame = run(frame, roi)
-    img_copy = img.copy()
-    img_h, img_w = img.shape[:2]
-    cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
-    cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
-
-    frame_resize = cv2.resize(img_copy, size, interpolation=cv2.INTER_NEAREST)
-    frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-
-    frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # Convert the image to LAB space
+    frame = img.copy()
+    frame_lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
 
     area_max = 0
     areaMaxContour = 0
     for i in color_range:
         if i in __target_color:
             detect_color = i
-            frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # Bitwise operation on the original image and mask
-            opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # Opening operation
-            closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # Closing operation
-            contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # Find contours
-            areaMaxContour, area_max = getAreaMaxContour(contours)  # Find the largest contour
-    if area_max > 2500:  # If the largest area is found
+            frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])
+            opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+            closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+            contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+            areaMaxContour, area_max = getAreaMaxContour(contours)
+    if area_max > 2500:
         rect = cv2.minAreaRect(areaMaxContour)
         box = np.int0(cv2.boxPoints(rect))
 
-        img_centerx, img_centery = getCenter(rect, roi, size, square_length)  # Get the center coordinates of the block
-        world_x, world_y = convertCoordinate(img_centerx, img_centery, size)  # Convert to real-world coordinates
+        img_centerx, img_centery = getCenter(rect, roi, size, square_length)
+        world_x, world_y = convertCoordinate(img_centerx, img_centery, size)
 
         cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
         cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1)  # Draw the center point
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1)
 
     return img
 
 if __name__ == '__main__':
-    init()
-    start()
     my_camera = Camera.Camera()
     my_camera.camera_open()
     while True:
         img = my_camera.frame
         if img is not None:
             frame = img.copy()
-            Frame = run(frame)
+            roi = None  # You need to define ROI or pass it here
+            Frame = run(frame, roi)
             cv2.imshow('Frame', Frame)
             key = cv2.waitKey(1)
             if key == 27:
