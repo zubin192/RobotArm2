9ca02a7a80cc88bb126a6cf3b4a564c030c55c2f
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-24 19:12:16 -0800

    8

--- ArmPi/Functions/W7Perception.py
+++ ArmPi/Functions/W7Perception.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 # coding=utf8
 import sys
+sys.path.append('/home/pi/ArmPi/')
 import cv2
+import time
+import Camera
+import threading
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
 import numpy as np
 
 __target_color = ('red',)
