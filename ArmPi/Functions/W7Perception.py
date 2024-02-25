98d355f05c56336709eb3ec8b4c4fc0c99ee6986
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-24 19:33:42 -0800

    23

--- ArmPi/Functions/W7Perception.py
+++ ArmPi/Functions/W7Perception.py
@@ -15,83 +15,47 @@ import numpy as np
 
 
 class ColorTracker:
+    range_rgb = {
+        'red': (0, 0, 255),
+        'blue': (255, 0, 0),
+        'green': (0, 255, 0),
+        'black': (0, 0, 0),
+        'white': (255, 255, 255),
+    }
+    
     def __init__(self):
         self.__target_color = ('red',)
         self.__isRunning = False
-        self.__roi = ()
-        self.__rect = None
-        self.__size = (640, 480)
-        self.__world_x, self.__world_y = 0, 0
-        self.__last_x, self.__last_y = 0, 0
-        self.__center_list = []
-        self.__count = 0
-        self.__start_count_t1 = True
-        self.__t1 = 0
+        self.rect = None
+        self.size = (640, 480)
+        self.rotation_angle = 0
+        self.world_X, self.world_Y = 0, 0
+        self.world_x, self.world_y = 0, 0
+        self.t1 = 0
+        self.roi = ()
+        self.get_roi = False
+        self.last_x, self.last_y = 0, 0
+        self.count = 0
+        self.track = False
+        self.center_list = []
+        self.start_count_t1 = False
 
-    def set_target_color(self, target_color):
+    def setTargetColor(self, target_color):
         self.__target_color = target_color
+        return (True, ())
 
     def start(self):
         self.__isRunning = True
+        print("ColorTracking Start")
 
     def stop(self):
         self.__isRunning = False
+        print("ColorTracking Stop")
 
-    def run(self, img):
-        if not self.__isRunning:
-            return img
-
-        frame_resize = cv2.resize(img.copy(), self.__size, interpolation=cv2.INTER_NEAREST)
-        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
-
-        area_max = 0
-        areaMaxContour = 0
-        for color in color_range:
-            if color in self.__target_color:
-                detect_color = color
-                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])
-                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
-                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
-                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
-                areaMaxContour, area_max = self.getAreaMaxContour(contours)
-
-        if area_max > 2500:
-            rect = cv2.minAreaRect(areaMaxContour)
-            box = np.int0(cv2.boxPoints(rect))
-            roi = self.getROI(box)
-
-            img_centerx, img_centery = self.getCenter(rect, roi, self.__size, square_length)
-            world_x, world_y = self.convertCoordinate(img_centerx, img_centery, self.__size)
-
-            cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
-            cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')',
-                        (min(box[0, 0], box[2, 0]), box[2, 1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, range_rgb[detect_color], 1)
-
-            distance = math.sqrt(pow(world_x - self.__last_x, 2) + pow(world_y - self.__last_y, 2))
-            self.__last_x, self.__last_y = world_x, world_y
-
-            if distance < 0.3:
-                self.__center_list.extend((world_x, world_y))
-                self.__count += 1
-                if self.__start_count_t1:
-                    self.__start_count_t1 = False
-                    self.__t1 = time.time()
-                if time.time() - self.__t1 > 1.5:
-                    rotation_angle = rect[2]
-                    self.__start_count_t1 = True
-                    self.__world_x, self.__world_y = np.mean(np.array(self.__center_list).reshape(self.__count, 2), axis=0)
-                    self.__count = 0
-                    self.__center_list = []
-            else:
-                self.__t1 = time.time()
-                self.__start_count_t1 = True
-                self.__count = 0
-                self.__center_list = []
-
-        return img
-
+    def exit(self):
+        self.__isRunning = False
+        print("ColorTracking Exit")
+        
     def getAreaMaxContour(self, contours):
         contour_area_temp = 0
         contour_area_max = 0
@@ -106,39 +70,85 @@ class ColorTracker:
 
         return area_max_contour, contour_area_max
 
-    def getROI(self, box):
-        # Implement your getROI function here
-        pass
+    def run(self, img):
+        img_copy = img.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
 
-    def getCenter(self, rect, roi, size, square_length):
-        # Implement your getCenter function here
-        pass
+        if not self.__isRunning:
+            return img
 
-    def convertCoordinate(self, img_centerx, img_centery, size):
-        # Implement your convertCoordinate function here
-        pass
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
 
+        if self.get_roi:
+            self.get_roi = False
+            frame_gb = self.getMaskROI(frame_gb, self.roi, self.size)
 
-def main():
-    tracker = ColorTracker()
-    tracker.start()
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
 
-    my_camera = Camera()
+        area_max = 0
+        areaMaxContour = 0
+        for i in self.range_rgb:
+            if i in self.__target_color:
+                detect_color = i
+                frame_mask = cv2.inRange(frame_lab, self.range_rgb[detect_color][0], self.range_rgb[detect_color][1])
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+                areaMaxContour, area_max = self.getAreaMaxContour(contours)
+            if area_max > 2500:
+                self.rect = cv2.minAreaRect(areaMaxContour)
+                box = np.int0(cv2.boxPoints(self.rect))
+
+                self.roi = self.getROI(box)
+                self.get_roi = True
+
+                img_centerx, img_centery = self.getCenter(self.rect, self.roi, self.size, square_length)
+                self.world_x, self.world_y = self.convertCoordinate(img_centerx, img_centery, self.size)
+
+                cv2.drawContours(img, [box], -1, self.range_rgb[detect_color], 2)
+                cv2.putText(img, '(' + str(self.world_x) + ',' + str(self.world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[detect_color], 1)
+                distance = math.sqrt(pow(self.world_x - self.last_x, 2) + pow(self.world_y - self.last_y, 2))
+                self.last_x, self.last_y = self.world_x, self.world_y
+                self.track = True
+                
+                if distance < 0.3:
+                    self.center_list.extend((self.world_x, self.world_y))
+                    self.count += 1
+                    if self.start_count_t1:
+                        self.start_count_t1 = False
+                        self.t1 = time.time()
+                    if time.time() - self.t1 > 1.5:
+                        self.rotation_angle = self.rect[2]
+                        self.start_count_t1 = True
+                        self.world_X, self.world_Y = np.mean(np.array(self.center_list).reshape(self.count, 2), axis=0)
+                        self.count = 0
+                        self.center_list = []
+                        self.start_pick_up = True
+                else:
+                    self.t1 = time.time()
+                    self.start_count_t1 = True
+                    self.count = 0
+                    self.center_list = []
+
+        return img
+
+if __name__ == '__main__':
+    start()
+    __target_color = ('red', )
+    my_camera = Camera.Camera()
     my_camera.camera_open()
-
     while True:
         img = my_camera.frame
         if img is not None:
             frame = img.copy()
-            frame = tracker.run(frame)
-            cv2.imshow('Frame', frame)
+            Frame = run(frame)
+            cv2.imshow('Frame', Frame)
             key = cv2.waitKey(1)
             if key == 27:
                 break
-
     my_camera.camera_close()
     cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    main()
