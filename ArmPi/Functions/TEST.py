a7ebc5204e92fa4f2c4a39e4745a4da637f86ea5
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-25 19:50:01 -0800

    3

--- ArmPi/Functions/TEST.py
+++ ArmPi/Functions/TEST.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # coding=utf8
 import sys
+sys.path.append('/home/pi/ArmPi/')
 import cv2
-import numpy as np
+import time
+import Camera
+import threading
 from LABConfig import *
 from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
-import math
+import numpy as np
 
 range_rgb = {
     'red': (0, 0, 255),
