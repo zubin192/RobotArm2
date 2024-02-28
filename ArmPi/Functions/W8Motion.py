3b7fa60c14f1ba5484df75d146612ba8363db4cd
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-28 13:16:34 -0800

    a

--- ArmPi/Functions/W8Motion.py
+++ ArmPi/Functions/W8Motion.py
@@ -19,7 +19,6 @@ servo3 = 500
 servo4 = 500
 servo5 = 500
 
-# Set the servo positions with a use_time of 1000ms
 Board.setBusServoPulse(1, servo1, 1000) # Servo 1
 Board.setBusServoPulse(2, servo2, 1000) # Servo 2
 Board.setBusServoPulse(3, servo3, 1000) # Servo 3
@@ -28,14 +27,12 @@ Board.setBusServoPulse(5, servo5, 1000) # Servo 5
 
 time.sleep(1)
 
-# Change the servo positions
-servo1 = 600
+servo1 = 500
 servo2 = 600
 servo3 = 600
 servo4 = 600
 servo5 = 600
 
-# Set the new servo positions with a use_time of 1000ms
 Board.setBusServoPulse(1, servo1, 1000) # Servo 1
 Board.setBusServoPulse(2, servo2, 1000) # Servo 2
 Board.setBusServoPulse(3, servo3, 1000) # Servo 3
