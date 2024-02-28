3cd3e337c05a23d726a0b65cf968b04705128e0a
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-28 13:19:14 -0800

    gers

--- ArmPi/Functions/W8Motion.py
+++ ArmPi/Functions/W8Motion.py
@@ -19,24 +19,21 @@ servo3 = 500
 servo4 = 500
 servo5 = 500
 
-Board.setBusServoPulse(1, servo1, 1000) # Servo 1
-Board.setBusServoPulse(2, servo2, 1000) # Servo 2
-Board.setBusServoPulse(3, servo3, 1000) # Servo 3
-Board.setBusServoPulse(4, servo4, 1000) # Servo 4
-Board.setBusServoPulse(5, servo5, 1000) # Servo 5
+Board.setBusServoPulse(1, servo1, 1000) 
+Board.setBusServoPulse(2, servo2, 1000) 
+Board.setBusServoPulse(3, servo3, 1000) 
+Board.setBusServoPulse(4, servo4, 1000) 
+Board.setBusServoPulse(5, servo5, 1000) 
 
 time.sleep(1)
 
-servo1 = 500
-servo2 = 600
-servo3 = 600
-servo4 = 600
-servo5 = 600
+servo1 = 600
 
-Board.setBusServoPulse(1, servo1, 1000) # Servo 1
-Board.setBusServoPulse(2, servo2, 1000) # Servo 2
-Board.setBusServoPulse(3, servo3, 1000) # Servo 3
-Board.setBusServoPulse(4, servo4, 1000) # Servo 4
-Board.setBusServoPulse(5, servo5, 1000) # Servo 5
+
+Board.setBusServoPulse(1, servo1, 1000) 
+Board.setBusServoPulse(2, servo2, 1000) 
+Board.setBusServoPulse(3, servo3, 1000) 
+Board.setBusServoPulse(4, servo4, 1000) 
+Board.setBusServoPulse(5, servo5, 1000) 
 
 time.sleep(1)
