0690f844737e5756b94ca65e743aed211abea441
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-02-28 13:14:08 -0800

    servo test

--- ArmPi/Functions/W8Motion.py
+++ ArmPi/Functions/W8Motion.py
@@ -1,25 +1,37 @@
-#!/usr/bin/python3
-# coding=utf8
-import sys
-sys.path.append('/home/pi/ArmPi/')
-import cv2
+# Import the necessary libraries
 import time
-import Camera
-import threading
-from LABConfig import *
-from ArmIK.Transform import *
-from ArmIK.ArmMoveIK import *
 import HiwonderSDK.Board as Board
-from CameraCalibration.CalibrationConfig import *
-import numpy as np
- 
-if sys.version_info.major == 2:
-    print('Please run this program with python3!')
-    sys.exit(0)
- 
-AK = ArmIK()
+
+# Initialize the servos
 servo1 = 500 
 servo2 = 500
 servo3 = 500
 servo4 = 500
 servo5 = 500
+
+# Set the servo positions
+Board.setBusServoPulse(1, servo1) # Servo 1
+Board.setBusServoPulse(2, servo2) # Servo 2
+Board.setBusServoPulse(3, servo3) # Servo 3
+Board.setBusServoPulse(4, servo4) # Servo 4
+Board.setBusServoPulse(5, servo5) # Servo 5
+
+# Wait for a while
+time.sleep(1)
+
+# Change the servo positions
+servo1 = 600
+servo2 = 600
+servo3 = 600
+servo4 = 600
+servo5 = 600
+
+# Set the new servo positions
+Board.setBusServoPulse(1, servo1) # Servo 1
+Board.setBusServoPulse(2, servo2) # Servo 2
+Board.setBusServoPulse(3, servo3) # Servo 3
+Board.setBusServoPulse(4, servo4) # Servo 4
+Board.setBusServoPulse(5, servo5) # Servo 5
+
+# Wait for a while
+time.sleep(1)
