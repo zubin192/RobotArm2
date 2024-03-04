f182be918f76ad742d36f5dc1915ac096707cc45
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-03 17:27:47 -0800

    camera fix

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -4,11 +4,12 @@ import sys
 sys.path.append('/home/pi/ArmPi/')
 import cv2
 import time
+import Camera
 import threading
-import HiwonderSDK.Board as Board
+from LABConfig import *
 from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
-from LABConfig import *
+import HiwonderSDK.Board as Board
 from CameraCalibration.CalibrationConfig import *
 import numpy as np
 
