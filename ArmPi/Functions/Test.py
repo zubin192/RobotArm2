add19a516e3bf12d41e886021ab5bea7e29d7bfe
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-03 16:49:50 -0800

    a

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # coding=utf8
-
 import sys
-import threading
-import time
+sys.path.append('/home/pi/ArmPi/')
 import cv2
-import numpy as np
-import HiwonderSDK.Board as Board
+import time
+import Camera
+import threading
+from LABConfig import *
 from ArmIK.Transform import *
 from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+import numpy as np
 
 class RoboticArm:
     def __init__(self):
