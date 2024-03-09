13fff5307283390481c13258f9f3c3f6c2e26b99
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-09 15:23:46 -0800

    s

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -6,11 +6,12 @@ class Perception:
     def __init__(self):
         self.size = (640, 480)
         self.my_camera = cv2.VideoCapture(0)  # Assuming camera index 0
-        self.drink_can_color_range = {'lower': np.array([0, 100, 100]), 'upper': np.array([10, 255, 255])}
+        # Define a wider range to capture various shades of white
+        self.white_color_range = {'lower': np.array([0, 0, 200]), 'upper': np.array([180, 50, 255])}
 
-    def detect_drink_can(self, frame):
+    def detect_white_object(self, frame):
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(frame_hsv, self.drink_can_color_range['lower'], self.drink_can_color_range['upper'])
+        mask = cv2.inRange(frame_hsv, self.white_color_range['lower'], self.white_color_range['upper'])
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
         if contours:
@@ -24,13 +25,13 @@ class Perception:
         while True:
             ret, frame = self.my_camera.read()
             if ret:
-                drink_can_bbox = self.detect_drink_can(frame)
-                if drink_can_bbox:
-                    x1, y1, x2, y2 = drink_can_bbox
+                white_obj_bbox = self.detect_white_object(frame)
+                if white_obj_bbox:
+                    x1, y1, x2, y2 = white_obj_bbox
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Draw a green bounding box
-                    print("Drink can found at coordinates:", (x1 + x2) // 2, (y1 + y2) // 2)
+                    print("White object found at coordinates:", (x1 + x2) // 2, (y1 + y2) // 2)
                 else:
-                    print("Drink can not found in the frame.")
+                    print("White object not found in the frame.")
                 
                 cv2.imshow('Frame', frame)
                 key = cv2.waitKey(1)
