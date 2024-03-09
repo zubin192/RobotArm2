c7e486670836cf55012b97c64b10860b88740007
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-09 15:49:30 -0800

    a

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -1,87 +1,57 @@
-#!/usr/bin/python3
+import sys
+sys.path.append('/home/pi/ArmPi/')
 import cv2
-import numpy as np
-import threading
-import time
+import Camera
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
 
-class Perception:
+class CircleDetector:
     def __init__(self):
-        self.size = (320, 240)  # Further reduced resolution
-        self.cap = cv2.VideoCapture(0)  # Open the default camera
-        self.scale_factor = 0.25  # Scale factor for resizing
-        self.skip_frames = 2  # Skip every other frame
-        self.frame_processed = True
-        self.lock = threading.Lock()
+        self.target_color = 'black'
+        self.camera = Camera.Camera()
+        self.camera.camera_open()
+        self.size = (640, 480)
 
-    def preprocess_frame(self, frame):
-        frame_resized = cv2.resize(frame, self.size, interpolation=cv2.INTER_AREA)
-        frame_gray = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2GRAY)
-        _, frame_threshold = cv2.threshold(frame_gray, 30, 255, cv2.THRESH_BINARY)  # Threshold to detect the black dot
-        frame_threshold = cv2.morphologyEx(frame_threshold, cv2.MORPH_CLOSE, np.ones((5, 5), np.uint8))  # Morphological closing to fill gaps
-        return frame_resized, frame_threshold
+    def process_frame(self, frame):
+        frame_resize = cv2.resize(frame, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
+        return frame_lab
 
-    def detect_black_circle(self, frame):
-        circles = cv2.HoughCircles(frame, cv2.HOUGH_GRADIENT, dp=1, minDist=30, param1=50, param2=20, minRadius=5, maxRadius=50)
-        
+    def draw_circles(self, frame, frame_lab):
+        frame_mask = cv2.inRange(frame_lab, color_range[self.target_color][0], color_range[self.target_color][1])
+        opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+        closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+        circles = cv2.HoughCircles(closed, cv2.HOUGH_GRADIENT, dp=1, minDist=100, param1=70, param2=20, minRadius=50, maxRadius=400)
+
+
         if circles is not None:
             circles = np.uint16(np.around(circles))
-            for circle in circles[0, :]:
-                x, y, r = circle
-                return (x - r, y - r, x + r, y + r)  # Return coordinates of the bounding box of the circle
-        else:
-            return None
+            for i in circles[0, :]:
+                center_x, center_y = convertCoordinate(i[0], i[1], self.size)
+                cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+                cv2.putText(frame, self.target_color, (i[0], i[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1)
+                cv2.putText(frame, f'x: {center_x:.2f}, y: {center_y:.2f}', (i[0], i[1] - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 1)
 
     def run(self):
         while True:
-            ret, frame = self.cap.read()
-            if ret:
-                frame_resized, frame_threshold = self.preprocess_frame(frame)
-                black_circle_bbox = self.detect_black_circle(frame_threshold)
-                
-                if black_circle_bbox:
-                    x1, y1, x2, y2 = black_circle_bbox
-                    x1 = int(x1 / self.scale_factor)
-                    y1 = int(y1 / self.scale_factor)
-                    x2 = int(x2 / self.scale_factor)
-                    y2 = int(y2 / self.scale_factor)
-                    with self.lock:
-                        cv2.circle(frame_resized, ((x1 + x2) // 2, (y1 + y2) // 2), (x2 - x1) // 2, (0, 255, 0), 2)  # Draw a green circle
-                
-                cv2.imshow('Frame', frame_resized)
-                
+            img = self.camera.frame
+            if img is not None:
+                frame = img.copy()
+                frame_lab = self.process_frame(frame)
+                self.draw_circles(frame, frame_lab)
+                cv2.imshow('Frame', frame)
                 key = cv2.waitKey(1)
-                if key == 27:
+                if key == 27:  # ESC key to break
                     break
-        self.cap.release()
+
+        self.camera.camera_close()
         cv2.destroyAllWindows()
 
-    def main_loop(self):
-        while True:
-            if self.frame_processed:
-                ret, frame = self.cap.read()
-                if ret:
-                    self.frame_processed = False
-                    t = threading.Thread(target=self.process_frame, args=(frame,))
-                    t.start()
-            else:
-                time.sleep(0.001)  # Sleep briefly to reduce CPU usage
-
-    def process_frame(self, frame):
-        frame_resized, frame_threshold = self.preprocess_frame(frame)
-        black_circle_bbox = self.detect_black_circle(frame_threshold)
-        
-        if black_circle_bbox:
-            x1, y1, x2, y2 = black_circle_bbox
-            x1 = int(x1 / self.scale_factor)
-            y1 = int(y1 / self.scale_factor)
-            x2 = int(x2 / self.scale_factor)
-            y2 = int(y2 / self.scale_factor)
-            with self.lock:
-                cv2.circle(frame_resized, ((x1 + x2) // 2, (y1 + y2) // 2), (x2 - x1) // 2, (0, 255, 0), 2)  # Draw a green circle
-        self.frame_processed = True
-
 if __name__ == '__main__':
-    perception = Perception()
-    t = threading.Thread(target=perception.run)
-    t.start()
-    perception.main_loop()
+    detector = CircleDetector()
+    detector.run()
