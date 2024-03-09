3e3c24867d91e00509bce1dbb40c7bf0531e35c6
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-09 15:16:29 -0800

    cans

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -1,41 +1,41 @@
+#!/usr/bin/python3
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
 
 class Perception:
     def __init__(self):
-        self.my_camera = Camera.Camera()
-        self.my_camera.camera_open()
-        self.model = load_model('path_to_your_model.h5')  # Load your trained model
+        self.size = (640, 480)
+        self.my_camera = cv2.VideoCapture(0)  # Assuming camera index 0
+        self.drink_can_color_range = {'lower': np.array([0, 100, 100]), 'upper': np.array([10, 255, 255])}
 
-    def run(self, img):
-        # Preprocess your image for your model
-        img_preprocessed = preprocess(img)
-
-        # Predict the bounding box of the can
-        bounding_box = self.model.predict(img_preprocessed)
-
-        # Draw the bounding box on the image
-        cv2.rectangle(img, (bounding_box[0], bounding_box[1]), (bounding_box[2], bounding_box[3]), (0, 255, 0), 2)
-
-        # Estimate the distance based on the size of the bounding box
-        distance = estimate_distance(bounding_box)
-
-        print('Distance:', distance)
-
-        return img
+    def detect_drink_can(self, frame):
+        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(frame_hsv, self.drink_can_color_range['lower'], self.drink_can_color_range['upper'])
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        if contours:
+            contour_area_max = max(contours, key=cv2.contourArea)
+            x, y, w, h = cv2.boundingRect(contour_area_max)
+            return (x + w // 2, y + h // 2)
+        else:
+            return None
 
     def main_loop(self):
         while True:
-            img = self.my_camera.frame
-            if img is not None:
-                frame = img.copy()
-                Frame = self.run(frame)
-                cv2.imshow('Frame', Frame)
+            ret, frame = self.my_camera.read()
+            if ret:
+                drink_can_pos = self.detect_drink_can(frame)
+                if drink_can_pos:
+                    print("Drink can found at coordinates:", drink_can_pos)
+                else:
+                    print("Drink can not found in the frame.")
+                
+                cv2.imshow('Frame', frame)
                 key = cv2.waitKey(1)
                 if key == 27:
                     break
-        self.my_camera.camera_close()
+        
+        self.my_camera.release()
         cv2.destroyAllWindows()
 
 if __name__ == '__main__':
