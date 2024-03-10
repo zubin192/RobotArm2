c777d8c17d607b46883ddc7238c2f308411ad1d5
zubin192 <87029029+zubin192@users.noreply.github.com>
2024-03-09 16:46:41 -0800

    se enter the commit message for your changes. Lines starting

--- ArmPi/Functions/Test.py
+++ ArmPi/Functions/Test.py
@@ -1,48 +1,66 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-import os
+#!/usr/bin/python3
+# coding=utf8
+
+import sys
 import time
-import json
-import pygame
-import requests
+import keyboard
+sys.path.append('/home/pi/ArmPi/')
+import HiwonderSDK.Board as Board
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
 
-url = "http://127.0.0.1:9030/jsonrpc"
-cmd = {
-    "method": "SetBusServoPulse",
-    "params": [],
-    "jsonrpc": "2.0",
-    "id": 0,
-}
+class RoboticArm:
+    def __init__(self):
+        self.servo1 = 500
+        self.AK = ArmIK()
 
-step_width = 10
+    def init_move(self):
+        Board.setBusServoPulse(1, self.servo1 - 50, 300)
+        Board.setBusServoPulse(2, 500, 500)
+        self.AK.setPitchRangeMoving((0, 10, 10), -30, 0, 90, 1500)
 
-# Map keyboard keys to servo actions
-key_map = {
-    pygame.K_LEFT: {"axis": 0, "increment": -step_width},
-    pygame.K_RIGHT: {"axis": 0, "increment": step_width},
-    pygame.K_UP: {"axis": 1, "increment": -step_width},
-    pygame.K_DOWN: {"axis": 1, "increment": step_width},
-    pygame.K_a: {"axis": 2, "increment": -step_width},
-    pygame.K_d: {"axis": 2, "increment": step_width},
-    pygame.K_w: {"axis": 3, "increment": -step_width},
-    pygame.K_s: {"axis": 3, "increment": step_width},
-}
+    def move_arm(self, target_position):
+        return self.AK.setPitchRangeMoving(target_position, -90, -90, 0, 1500)
 
-connected = False
-change = [500, 500, 136, 931, 795, 500]
+    def open_gripper(self):
+        Board.setBusServoPulse(1, self.servo1 - 280, 500)
 
-pygame.init()
-pygame.display.set_mode((100, 100))  # Create a window for keyboard events
+    def close_gripper(self):
+        Board.setBusServoPulse(1, self.servo1, 500)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key in key_map:
-                axis = key_map[event.key]["axis"]
-                increment = key_map[event.key]["increment"]
-                change[axis] += increment
-                change[axis] = max(0, min(1000, change[axis]))
-                cmd["params"] = [20, 1, axis + 1, change[axis]]
-                r = requests.post(url, json=cmd).json()
+def main():
+    robotic_arm = RoboticArm()
+    robotic_arm.init_move()
+    time.sleep(2)
 
-    time.sleep(0.06)
+    print("Control the arm using the keyboard:")
+    print("w/s: Increase/decrease x-coordinate")
+    print("a/d: Increase/decrease y-coordinate")
+    print("q/e: Increase/decrease z-coordinate")
+    print("Press 'esc' to exit")
+
+    x, y, z = 0, 0, 0
+
+    def change_coordinates(e):
+        nonlocal x, y, z
+        if e.name == 'w': x += 100
+        elif e.name == 's': x -= 100
+        elif e.name == 'a': y += 100
+        elif e.name == 'd': y -= 100
+        elif e.name == 'q': z += 100
+        elif e.name == 'e': z -= 100
+
+        target_position = (x, y, z)
+        robotic_arm.move_arm(target_position)
+
+    keyboard.on_press_key("w", change_coordinates)
+    keyboard.on_press_key("s", change_coordinates)
+    keyboard.on_press_key("a", change_coordinates)
+    keyboard.on_press_key("d", change_coordinates)
+    keyboard.on_press_key("q", change_coordinates)
+    keyboard.on_press_key("e", change_coordinates)
+
+    keyboard.wait('esc')
+
+if __name__ == '__main__':
+    main()
